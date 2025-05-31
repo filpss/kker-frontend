a310cf7f268a1bdce37ad9a5cd1d64b4041232b9
filpss <filipepinho2010@hotmail.com>
2025-05-31 18:33:20 -0300

    refactor: Adjusting the project structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "../src/styles/index.css";
 import Header from "./components/Header";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <Header />
-  </StrictMode>
-);
+function App() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
+
+export default App;
