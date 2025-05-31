fd7563d0d13223ae20076ee88a73ebf67418cba8
filpss <filipepinho2010@hotmail.com>
2025-05-31 18:36:37 -0300

    feat: Add footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <>
+      <footer className="fixed bottom-0 left-0 w-full text-[#c6c6c6] text-center p-4 pb-10">
+        <p>© 2024 Filipe Pinho. Todos os direitos reservados.</p>
+      </footer>
+    </>
+  );
+}
+
+export default Footer;
